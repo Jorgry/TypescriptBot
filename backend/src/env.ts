1eb1d9a9322b7c4395e4e7a1e7a01b1134adfa2a
Jorge <jorgitoadame17@gmail.com>
2025-07-14 02:43:46 -0700

    Added Bot token to enviroment variables

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,10 +1,12 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
+import { DISCORD_APPLCAITION_SECRET } from "./constants";
 
 export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
     DISCORD_APPLICATION_ID: z.string(),
+    DISCORD_APPLICATION_TOKEN: z.string(),
     DISCORD_APPLICATION_SECRET: z.string(),
     DISCORD_REDIRECT_URI: z.string(),
   },
